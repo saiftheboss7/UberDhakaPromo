fac592508c631f403d9319db7770c4609708e9e7
Saif Hassan <saiftheboss7@gmail.com>
2017-02-18 15:10:59 +0600

    Inital Push

---
+++ uberpromocodes/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Promos
+from django.utils import timezone
+# Create your views here.
+def home(request):
+	posts = Promos.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'index.html', {'posts': posts})
